24f27740600041da83060e3096fde082d92092f4
CarlosReyesMelero <creyesmelero@gmail.com>
2024-07-22 23:37:36 +0200

    Read from the LocalStorage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -13,7 +13,9 @@ export class GifsService {
   private apiKey: string = 'Pog0qsXNZ4t7DGfRDN0Kq9MapRCYvNjq'
   private serviceUrl: string = 'https://api.giphy.com/v1/gifs'
 
-  constructor( private http: HttpClient) {}
+  constructor( private http: HttpClient) {
+    this.loadLocalStorage();
+  }
 
   get tagsHistory() {
     return [...this._tagsHistory];
@@ -33,6 +35,16 @@ export class GifsService {
 
   private saveLocalStorage():void {
     localStorage.setItem('history', JSON.stringify(this._tagsHistory));
+  }
+
+  private loadLocalStorage():void {
+
+    if (!localStorage.getItem('history')) return;
+
+    this._tagsHistory = JSON.parse(localStorage.getItem('history')!);
+
+    if( this._tagsHistory.length === 0) return;
+    this.searchTag(this._tagsHistory[0]);
   }
 
   searchTag(tag: string): void {
