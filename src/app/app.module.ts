ea3d40f837c99414dbf3bc61d46542226ed4c4d5
CarlosReyesMelero <creyesmelero@gmail.com>
2024-07-22 17:01:40 +0200

    HTTP petition

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
 
 import { GifsModule } from './gifs/gifs.module';
 import { SharedModule } from './shared/shared.module';
@@ -7,6 +8,7 @@ import { SharedModule } from './shared/shared.module';
 // import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './gifs/pages/home/home-page.component';
+
 
 @NgModule({
   declarations: [
@@ -16,6 +18,7 @@ import { HomePageComponent } from './gifs/pages/home/home-page.component';
     // AppRoutingModule,
     BrowserModule,
     GifsModule,
+    HttpClientModule,
     SharedModule,
   ],
   providers: [],
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,12 +1,16 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GifsService {
-  private _tagsHistory: string[] = [];
 
-  constructor() {}
+  private _tagsHistory: string[] = [];
+  private apiKey: string = 'Pog0qsXNZ4t7DGfRDN0Kq9MapRCYvNjq'
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs'
+
+  constructor( private http: HttpClient) {}
 
   get tagsHistory() {
     return [...this._tagsHistory];
@@ -27,6 +31,22 @@ export class GifsService {
     if (tag.length === 0) return;
 
     this.organizeHistory(tag.trim().toLowerCase());
+
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', '10')
+      .set('q', tag)
+
+    this.http.get(`${this.serviceUrl}/search`, { params })
+      .subscribe( resp => {
+        console.log(resp);
+
+    });
+
+    // fetch('https://api.giphy.com/v1/gifs/search?api_key=Pog0qsXNZ4t7DGfRDN0Kq9MapRCYvNjq&q=valorant&limit=10')
+    //   .then( resp => resp.json())
+    //   .then( data => console.log(data));
 
     // this._tagsHistory.unshift(tag);
   }
